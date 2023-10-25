6c4f0c35f34737b626feee3da0880c96afe40f18
David Rosero Calle <davidroserocalle@gmail.com>
2023-10-24 21:36:49 -0500

    Finalizacion del footer y de la pagina tipo marketing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { ThemeProvider } from '@/components/providers/theme-provider'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -29,8 +30,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+          storageKey='jotion-theme-2'
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
